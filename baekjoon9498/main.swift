81403b788d0cb30b4cc4a9a46cb47a264bfdd3e7
hamgui-2022 <ohjhlee14@gachon.ac.kr>
2023-04-05 21:27:30 +0900

    add baekjoon9498

---
+++ baekjoon9498/main.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  baekjoon9498
+//
+//  Created by 이재혁 on 2023/04/05.
+//
+
+import Foundation
+
+let score = Int(readLine()!)!
+
+if (score >= 90) {print("A")}
+else if (score >= 80) {print("B")}
+else if (score >= 70) {print("C")}
+else if (score >= 60) {print("D")}
+else {print("F")}
+
