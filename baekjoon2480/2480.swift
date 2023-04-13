61c63315f08ca41c00fe4decada350cfd1e12656
hamgui-2022 <ohjhlee14@gachon.ac.kr>
2023-04-13 22:17:33 +0900

    edit baekjoon2480/2480.swift

--- baekjoon2480/2480.swift
+++ baekjoon2480/2480.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+let inputArray = readLine()!.split(separator: " ").map { Int(String($0))! }
+
+var prize : Int
+
+if inputArray[0] != inputArray[1] && inputArray[1] != inputArray[2] && inputArray[0] != inputArray[2] {
+    prize = max(inputArray[0], max(inputArray[1], inputArray[2])) * 100
+}
+else if inputArray[0] == inputArray[1] && inputArray[0] == inputArray[2] {
+    prize = 10000 + inputArray[0] * 1000
+}
+else {
+    if inputArray[0] == inputArray[1] || inputArray[0] == inputArray[2] {
+        prize = inputArray[0] * 100 + 1000
+    }
+    else {
+        prize = inputArray[1] * 100 + 1000
+    }
+}
+
+print(prize)
