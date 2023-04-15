8febbc02d6357587babd0148d3a700071a454618
hamgui-2022 <ohjhlee14@gachon.ac.kr>
2023-04-15 22:46:22 +0900

    add baekjoon8393

---
+++ baekjoon8393/8393.swift
@@ -0,0 +1,16 @@
+//
+//  8393.swift
+//  Algorithm
+//
+//  Created by 이재혁 on 2023/04/15.
+//
+
+import Foundation
+let inputNum: Int = Int(readLine()!)!
+var sum: Int = 0
+
+for i in 1...inputNum {
+    sum += i
+}
+
+print(sum)
