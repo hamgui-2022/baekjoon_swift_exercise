d3f9fb421b1c09c0ec3117442fb3a5f33390e509
hamgui-2022 <ohjhlee14@gachon.ac.kr>
2023-04-19 22:38:00 +0900

    add baekjoon25314

---
+++ baekjoon25314/25314.swift
@@ -0,0 +1,16 @@
+//
+//  25314.swift
+//  Algorithm
+//
+//  Created by 이재혁 on 2023/04/19.
+//
+
+import Foundation
+let byte : Int = Int(readLine()!)!
+
+let number : Int = byte / 4
+
+for _ in 1...number {
+    print("long", terminator: " ")
+}
+print("int")
