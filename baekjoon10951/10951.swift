69709c90f6c5556e22a411297d573efcd0aabb8b
hamgui-2022 <ohjhlee14@gachon.ac.kr>
2023-04-25 00:54:18 +0900

    add baekjoon10951

---
+++ baekjoon10951/10951.swift
@@ -0,0 +1,15 @@
+//
+//  10951.swift
+//  Algorithm
+//
+//  Created by 이재혁 on 2023/04/25.
+//
+
+import Foundation
+
+while let input1 = readLine() {
+    var arr = input1.split(separator: " ")
+    var a = Int(arr[0])!
+    var b = Int(arr[1])!
+    print(a+b)
+}
