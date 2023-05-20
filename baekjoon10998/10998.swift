4533ae4e206cfbfe20bd90cc2f22f3c04fc4593d
hamgui-2022 <ohjhlee14@gachon.ac.kr>
2023-05-20 23:33:29 +0900

    add baekjoon10998

---
+++ baekjoon10998/10998.swift
@@ -0,0 +1,11 @@
+//
+//  10998.swift
+//  Algorithm
+//
+//  Created by 이재혁 on 2023/05/20.
+//
+
+import Foundation
+let inputArray = readLine()!.split(separator: " ").map { Int(String($0))! }
+
+print(inputArray[0] * inputArray[1])
