327a7e5d569854ae262aa57097be07b429d8f2a0
hamgui-2022 <ohjhlee14@gachon.ac.kr>
2023-05-27 22:28:57 +0900

    add baekjoon24723

---
+++ baekjoon24723/24723.swift
@@ -0,0 +1,12 @@
+//
+//  24723.swift
+//  Algorithm
+//
+//  Created by 이재혁 on 2023/05/27.
+//
+
+import Foundation
+let inputNum = Int(readLine()!)!
+
+let output = Int(pow(2.0, Double(inputNum)))
+print(output)
