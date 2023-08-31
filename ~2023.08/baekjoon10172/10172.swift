8e46f843899fd119980dbab783d19bfbd6ce6d78
hamgui-2022 <ohjhlee14@gachon.ac.kr>
2023-08-31 21:50:55 +0900

    폴더정리, add baekjoon10172

---
+++ ~2023.08/baekjoon10172/10172.swift
@@ -0,0 +1,18 @@
+//
+//  10172.swift
+//  Algorithm
+//
+//  Created by 이재혁 on 2023/08/31.
+//
+
+import Foundation
+
+print("|\\_/|")
+
+print("|q p|   /}")
+
+print("( 0 )\"\"\"\\")
+
+print("|\"^\"`    |")
+
+print("||_/=\\\\__|")
