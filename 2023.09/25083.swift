b1b91d0ac72be0a541d93f345ae70ffd8cd31ee1
hamgui-2022 <ohjhlee14@gachon.ac.kr>
2023-09-05 15:26:49 +0900

    백준 25083

---
+++ 2023.09/25083.swift
@@ -0,0 +1,18 @@
+//
+//  25083.swift
+//  Algorithm
+//
+//  Created by 이재혁 on 2023/09/05.
+//
+
+import Foundation
+
+let result = #"""
+         ,r'"7
+r`-_   ,'  ,/
+ \. ". L_r'
+   `~\/
+      |
+      |
+"""#
+print(result)
