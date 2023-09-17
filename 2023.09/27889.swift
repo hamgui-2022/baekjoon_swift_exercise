b20c130ecb2c3ba2c12039161d27297e4ad93c45
hamgui-2022 <ohjhlee14@gachon.ac.kr>
2023-09-17 20:38:32 +0900

    백준 27889

---
+++ 2023.09/27889.swift
@@ -0,0 +1,16 @@
+
+let input = readLine()!
+
+switch input {
+case "NLCS":
+    print("North London Collegiate School")
+case "BHA":
+    print("Branksome Hall Asia")
+case "KIS":
+    print("Korea International School")
+case "SJA":
+    print("St. Johnsbury Academy")
+default:
+    print("")
+
+}
