a4a4aca903ff2ab2cd854092d49b013626a21d06
hamgui-2022 <ohjhlee14@gachon.ac.kr>
2023-10-15 23:38:24 +0900

    백준 2742

---
+++ 2023.10/2742.swift
@@ -0,0 +1,14 @@
+//
+//  2742.swift
+//  Algorithm
+//
+//  Created by 이재혁 on 10/15/23.
+//
+
+let input: Int = Int(readLine()!)!
+var cnt: Int = input
+
+for _ in 1...input {
+    print(cnt)
+    cnt -= 1
+}
