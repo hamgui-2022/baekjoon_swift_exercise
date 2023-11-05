07ff32de3a414062b5383edd09e02b008b978eaa
hamgui-2022 <ohjhlee14@gachon.ac.kr>
2023-11-05 23:57:49 +0900

    .

---
+++ sorry/optional.swift
@@ -0,0 +1,17 @@
+//
+//  optional.swift
+//  Algorithm
+//
+//  Created by 이재혁 on 2023/04/15.
+//
+
+import Foundation
+
+var a : String? = "abc"
+var b : Int? = 4
+
+a = nil
+b = nil
+
+print(a)
+print(b)
