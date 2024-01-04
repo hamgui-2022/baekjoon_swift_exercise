4b2a816493d343e7de3aeae0f8649c9794999707
hamgui-2022 <ohjhlee14@gachon.ac.kr>
2024-01-04 22:57:12 +0900

    20240104 swift 2163

---
+++ 2024.01/2163.swift
@@ -0,0 +1,9 @@
+//
+//  2163.swift
+//  Algorithm
+//
+//  Created by 이재혁 on 1/4/24.
+//
+
+let inputArray = readLine()!.split(separator: " ").map { Int(String($0))! }
+print(inputArray[0] * inputArray[1] - 1)
