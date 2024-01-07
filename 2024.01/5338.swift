a573818d78b3309a98195ca1e52238793454f523
hamgui-2022 <ohjhlee14@gachon.ac.kr>
2024-01-07 10:28:08 +0900

    20240107 swift 5338

---
+++ 2024.01/5338.swift
@@ -0,0 +1,13 @@
+//
+//  5338.swift
+//  Algorithm
+//
+//  Created by 이재혁 on 1/7/24.
+//
+
+import Foundation
+print("       _.-;;-._")
+print("'-..-'|   ||   |")
+print("'-..-'|_.-;;-._|")
+print("'-..-'|   ||   |")
+print("'-..-'|_.-''-._|")
